65e1bfb65c49d582d86814faf40517609df52e8f
prashu1605 <158436071+prashu1605@users.noreply.github.com>
2025-04-28 15:00:01 +0530

    GAE.py

--- GAE.py
+++ GAE.py
@@ -1,1 +1,6 @@
 print("welcome to my project ")
+print("we will perform simple math calculation")
+a=input("enter first number")
+b=input("enter second number")
+c=a+b;
+print("the addition is : "+c)
